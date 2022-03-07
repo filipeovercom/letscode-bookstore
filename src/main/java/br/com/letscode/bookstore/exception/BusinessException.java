707efecdbb8d965298f1c1ce65cfa5c04d3d3932
Filipe Damasceno Abreu <filipeovercom@gmail.com>
2022-03-06 23:44:08 -0300

    Refactoring to eliminate a issue of sonar analisys

--- src/main/java/br/com/letscode/bookstore/exception/BusinessException.java
+++ src/main/java/br/com/letscode/bookstore/exception/BusinessException.java
@@ -2,19 +2,11 @@ package br.com.letscode.bookstore.exception;
 
 public class BusinessException extends RuntimeException {
 
-    private int type;
+    private final int type;
 
     public BusinessException(int type, String message) {
         super(message);
         this.type = type;
-    }
-
-    public BusinessException(String message) {
-        super(message);
-    }
-
-    public BusinessException(String message, Throwable cause) {
-        super(message, cause);
     }
 
     public int getType() {
